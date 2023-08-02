7c237e47ce456b215ba9c686266d25aea0b9befc
jamesdurai <jamesdurai@outlook.com>
2023-08-02 15:07:49 +0530

    Using JavaScript

---
+++ main.js
@@ -0,0 +1,59 @@
+const startButton = document.getElementById('start-button');
+startButton.addEventListener('click', function() {
+    location.reload(); // This will refresh the page
+});
+
+let playGame = confirm("shall we play a rock, paper or scissors?");
+
+if (playGame){
+
+    let playerChoice = prompt("Please Enter rock, paper or scissors.");
+
+    if (playerChoice) {
+
+        let playerOne = playerChoice.trim().toLocaleLowerCase();
+
+        if (
+            playerOne === "rock" ||
+            playerOne === "paper" ||
+            playerOne === "scissors"
+        ){
+            let  computerChoice = Math.floor(Math.random() * 3 +1);
+            let computer =
+            computerChoice === 1 ? "rock":
+    
+            computerChoice === 2 ? "paper":
+
+            "sessiors";
+
+            let result =
+             playerOne === computer
+            ? "Tie Game":
+
+            playerOne === "paper" && computer === "rock"
+            ? `You: ${playerOne}\nComputer: ${computer}\nYou wins!`:
+
+            playerOne === "scissors" && computer === "paper"
+            ? `You: ${playerOne}\nComputerr: ${computer}\nYou wins!`:
+
+            playerOne === "rock" && computer === "scissors"
+            ? `You: ${playerOne}\nComputer: ${computer}\nYou wins!`:
+
+            
+             `You: ${playerOne}\nComputer: ${computer}\ncomputer wins!`;
+
+             alert(result);
+             let playAgain = confirm ("play again");
+
+             playAgain ? location.reload() : alert ("ok, thanks for playing");
+        }
+        else{
+            alert("you didn't enter rock, paper or sessiors");
+        }   
+    } else{
+        alert("I guess you changed your mind. May be Next Time");
+    }
+}
+else{
+    alert("ok may be next time");
+}
